aaa57a9e707b3046c35a95a1f8280581f626053c
ShawnShoper <xiehao,930825>
2018-08-16 17:01:52 +0800

    init

---
+++ src/main/java/org/alittlebitch/fitness/Application.java
@@ -0,0 +1,15 @@
+package org.alittlebitch.fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ShawnShoper
+ * @date 2018/8/16 17:01
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
